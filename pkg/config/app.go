48bed8ba6513a49afe5e0d8b18dd48db30daa8ea
toluwase <toluwasethomas1@gmail.com>
2021-09-26 01:35:26 +0100

    database connectivity and models

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,3 +1,19 @@
 package config
 
+import (
+	"github.com/jinzhu/gorm"
+)
+var (
+	db *gorm.DB
+)
+func Connect() {
+	d, err:= gorm.Open("mysql", "root:toluwase@tcp(127.0.0.1:3306)/golang-bookstore")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
 
+func GetDb() *gorm.DB{
+	return db
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,31 @@
 package models
+
+import (
+	"BOOK-MANAGEMENT-SYSTEM/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:""publication`
+}
+
+func init(){
+	config.Connect()
+	db = config.GetDb()
+	db.AutoMigrate(&Book{})
+}
+/*
+Control first is in the routes
+The routes gives the control to the controller
+Controllers will give control to the model (Book.go)
+
+1. User interacts with the route
+2. The routes send control to the controller (where we have our logic)
+3. Controllers now performs the operation with the database.
+the operations of the database has to reside in the models file books.go
+ */
